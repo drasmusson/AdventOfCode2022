26ce301c02245ac2daf14f8d51ac0b786f139c00
Daniel Rasmusson <daniel.rasmusson@hogia.se>
2022-12-08 16:47:14 +0100

    Day 8.

---
+++ Day08/Program.cs
@@ -0,0 +1,35 @@
+﻿// https://adventofcode.com/2022/day/8
+
+var input = File.ReadAllLines("Day08.txt");
+
+PartOne(input);
+
+void PartOne(string[] input)
+{
+    var xLength = input[0].Length;
+    var yLength = input.Length;
+
+    for (int y = 0; y < yLength; y++)
+    {
+        for (int x = 0; x < xLength; x++)
+        {
+
+        }
+    }
+}
+
+Console.WriteLine("Hello, World!");
+
+public record Coord(int X, int Y);
+
+public class Tree
+{
+    public bool Visible { get; set; }
+    public int Height { get; }
+
+    public Tree(int height)
+    {
+        Height = height;
+        Visible = false;
+    }
+}
