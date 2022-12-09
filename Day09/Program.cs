034e154497250a469b1c738522359edf41e9915c
Daniel <d.o.rasmusson@gmail.com>
2022-12-09 17:13:08 +0100

    Day 9 part two done and refactored part one to use part two solution.

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -9,7 +9,7 @@ void PartOne(string[] input)
 {
     var instructions = input.Select(x => (Direction: x.Split(' ')[0], Distance: int.Parse(x.Split(' ')[1]))).ToList();
 
-    var rope = new Rope();
+    var rope = new Rope(2);
 
     foreach (var instruction in instructions)
         rope.Move(instruction);
@@ -21,12 +21,12 @@ void PartTwo(string[] input)
 {
     var instructions = input.Select(x => (Direction: x.Split(' ')[0], Distance: int.Parse(x.Split(' ')[1]))).ToList();
 
-    var rope = new Rope();
+    var rope = new Rope(10);
 
     foreach (var instruction in instructions)
         rope.Move(instruction);
 
-    Console.WriteLine($"Part one: {rope.Visited.Count()}");
+    Console.WriteLine($"Part two: {rope.Visited.Count()}");
 }
 
 record Coord(int X, int Y)
@@ -43,15 +43,34 @@ record Coord(int X, int Y)
 
 class Rope
 {
-    public Coord Head { get; internal set; }
-    public Coord Tail { get; internal set; }
+    public Knot Head { get; internal set; }
     public HashSet<Coord> Visited { get; }
 
-    public Rope()
+    public Rope(int ropeLength)
     {
-        Head = new Coord(0, 0);
-        Tail = new Coord(0, 0);
         Visited = new HashSet<Coord> { new Coord(0, 0) };
+        Head = AddKnotsToRope(ropeLength);
+    }
+
+    private Knot AddKnotsToRope(int ropeLength)
+    {
+        var knots = new List<Knot>();
+
+        for (int i = 0; i < ropeLength; i++)
+        {
+            var knot = new Knot(null, null);
+
+            if (knots.Any())
+            {
+                knot.Parent = knots[i - 1];
+            }
+            knots.Add(knot);
+        }
+        for (int i = 0; i < knots.Count - 1; i++)
+        {
+            knots[i].Child = knots[i + 1];
+        }
+        return knots[0];
     }
 
     internal void Move((string Direction, int Distance) instruction)
@@ -66,10 +85,11 @@ class Rope
     {
         MoveHead(direction);
 
-        if(!HeadAndTailIsTouching())
-            MoveTail();
-
-        Visited.Add(Tail);
+        var tailCoord = Head.Child?.Move();
+        if (tailCoord != null) 
+        {
+            Visited.Add(tailCoord);
+        }
     }
 
     private void MoveHead(string direction)
@@ -77,151 +97,70 @@ class Rope
         switch (direction)
         {
             case "R":
-                Head = Head.RightNeighbour();
+                Head.Position = Head.Position.RightNeighbour();
                 break;
             case "L":
-                Head = Head.LeftNeighbour();
+                Head.Position = Head.Position.LeftNeighbour();
                 break;
             case "U":
-                Head = Head.TopNeighbour();
+                Head.Position = Head.Position.TopNeighbour();
                 break;
             case "D":
-                Head = Head.BottNeighbour();
+                Head.Position = Head.Position.BottNeighbour();
                 break;
             default:
                 break;
         }
-    }
-
-    private void MoveTail()
-    {
-        if (Tail.X - Head.X < -1 || (Tail.X - Head.X == -1 && Tail.Y != Head.Y))
-            Tail = Tail.RightNeighbour();
-        if (Tail.X - Head.X > 1 || (Tail.X - Head.X == 1 && Tail.Y != Head.Y))
-            Tail = Tail.LeftNeighbour();
-        if (Tail.Y - Head.Y < -1 || (Tail.Y - Head.Y == -1 && Tail.X != Head.X))
-            Tail = Tail.TopNeighbour();
-        if (Tail.Y - Head.Y > 1 || (Tail.Y - Head.Y == 1 && Tail.X != Head.X))
-            Tail = Tail.BottNeighbour();
-    }
-
-    private bool HeadAndTailIsTouching()
-    {
-        if (Head == Tail)
-            return true;
-
-        if (Tail == Head.BottNeighbour()
-            || Tail == Head.BottRightNeighbour()
-            || Tail == Head.BottLeftNeighbour()
-            || Tail == Head.LeftNeighbour()
-            || Tail == Head.RightNeighbour()
-            || Tail == Head.TopLeftNeighbour()
-            || Tail == Head.TopNeighbour()
-            || Tail == Head.TopRightNeighbour())
-            return true;
-
-        return false;
-    }
-}
-
-class LargeRope
-{
-    public Coord Head { get; internal set; }
-    public Knot ChildKnot { get; internal set; }
-    public HashSet<Coord> Visited { get; }
-
-    public LargeRope()
-    {
-        Head = new Coord(0, 0);
-        Visited = new HashSet<Coord> { new Coord(0, 0) };
-
-        var prevKnot = new Knot(null);
-
-        for (int i = 0; i < 10; i++)
-        {
-            var newKnot = new Knot(prevKnot); 
-            ChildKnot = newKnot;
-            prevKnot = newKnot;
-        }
-    }
-
-    internal void Move((string Direction, int Distance) instruction)
-    {
-        for (int i = 0; i < instruction.Distance; i++)
-        {
-            MoveInDirection(instruction.Direction);
-        }
-    }
-
-    private void MoveInDirection(string direction)
-    {
-        MoveHead(direction);
-
-        if (!HeadAndTailIsTouching())
-            MoveTail();
-
-        Visited.Add(Tail);
-    }
-
-    private void MoveHead(string direction)
-    {
-        switch (direction)
-        {
-            case "R":
-                Head = Head.RightNeighbour();
-                break;
-            case "L":
-                Head = Head.LeftNeighbour();
-                break;
-            case "U":
-                Head = Head.TopNeighbour();
-                break;
-            case "D":
-                Head = Head.BottNeighbour();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void MoveTail()
-    {
-        if (Tail.X - Head.X < -1 || (Tail.X - Head.X == -1 && Tail.Y != Head.Y))
-            Tail = Tail.RightNeighbour();
-        if (Tail.X - Head.X > 1 || (Tail.X - Head.X == 1 && Tail.Y != Head.Y))
-            Tail = Tail.LeftNeighbour();
-        if (Tail.Y - Head.Y < -1 || (Tail.Y - Head.Y == -1 && Tail.X != Head.X))
-            Tail = Tail.TopNeighbour();
-        if (Tail.Y - Head.Y > 1 || (Tail.Y - Head.Y == 1 && Tail.X != Head.X))
-            Tail = Tail.BottNeighbour();
-    }
-
-    private bool HeadAndTailIsTouching()
-    {
-        if (Head == Tail)
-            return true;
-
-        if (Tail == Head.BottNeighbour()
-            || Tail == Head.BottRightNeighbour()
-            || Tail == Head.BottLeftNeighbour()
-            || Tail == Head.LeftNeighbour()
-            || Tail == Head.RightNeighbour()
-            || Tail == Head.TopLeftNeighbour()
-            || Tail == Head.TopNeighbour()
-            || Tail == Head.TopRightNeighbour())
-            return true;
-
-        return false;
     }
 }
 
 class Knot
 {
-    public Coord Position { get; }
+    public Coord Position { get; internal set; }
+    public Knot? Parent { get; set; }
     public Knot? Child { get; set; }
-    public Knot(Knot? child)
+    public Knot(Knot? child, Knot? parent)
     {
         Child = child;
         Position = new Coord(0, 0);
+        Parent = parent;
+    }
+
+    internal Coord? Move()
+    {
+        if (IsTouchingParent())
+            return null;
+
+        if (Position.X - Parent?.Position.X < -1 || (Position.X - Parent?.Position.X == -1 && Position.Y != Parent?.Position.Y))
+            Position = Position.RightNeighbour();
+        if (Position.X - Parent?.Position.X > 1 || (Position.X - Parent?.Position.X == 1 && Position.Y != Parent?.Position.Y))
+            Position = Position.LeftNeighbour();
+        if (Position.Y - Parent?.Position.Y < -1 || (Position.Y - Parent?.Position.Y == -1 && Position.X != Parent?.Position.X))
+            Position = Position.TopNeighbour();
+        if (Position.Y - Parent?.Position.Y > 1 || (Position.Y - Parent?.Position.Y == 1 && Position.X != Parent?.Position.X))
+            Position = Position.BottNeighbour();
+
+        if (Child is null)
+            return Position;
+
+        return Child.Move();
+    }
+
+    private bool IsTouchingParent()
+    {
+        if (Position == Parent?.Position)
+            return true;
+
+        if (Position == Parent?.Position.BottNeighbour()
+            || Position == Parent?.Position.BottRightNeighbour()
+            || Position == Parent?.Position.BottLeftNeighbour()
+            || Position == Parent?.Position.LeftNeighbour()
+            || Position == Parent?.Position.RightNeighbour()
+            || Position == Parent?.Position.TopLeftNeighbour()
+            || Position == Parent?.Position.TopNeighbour()
+            || Position == Parent?.Position.TopRightNeighbour())
+            return true;
+
+        return false;
     }
 }
