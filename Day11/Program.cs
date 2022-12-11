c4dc80f2a481f626049436d0cf184499b1677961
Daniel <d.o.rasmusson@gmail.com>
2022-12-11 18:01:00 +0100

    Day 11 part two.

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -4,14 +4,15 @@ using System.Data;
 
 var input = File.ReadAllLines("Day11.txt");
 
-//PartOne(input);
+PartOne(input);
 PartTwo(input);
 
 void PartOne(string[] input)
 {
-    List<Monkey> monkeys = ParseMonkeys(input);
+    List<Monkey> monkeys = ParseMonkeys(input, true);
+    var divisor = GetDivisor(monkeys);
 
-    RunRounds(20, monkeys);
+    RunRounds(20, monkeys, divisor);
 
     var mostActive = monkeys.OrderByDescending(x => x.MbLevel).Take(2).Select(y => y.MbLevel).ToList();
     var totalMb = mostActive.Aggregate((x, y) => x * y);
@@ -20,56 +21,49 @@ void PartOne(string[] input)
 
 void PartTwo(string[] input)
 {
-    List<Monkey> monkeys = ParseMonkeys(input);
+    List<Monkey> monkeys = ParseMonkeys(input, false);
+    var divisor = GetDivisor(monkeys);
 
-    RunRounds2(10000, monkeys);
+    RunRounds(10000, monkeys, divisor);
 
     var mostActive = monkeys.OrderByDescending(x => x.MbLevel).Take(2).Select(y => y.MbLevel).ToList();
     var totalMb = mostActive.Aggregate((x, y) => x * y);
-    Console.WriteLine($"Part one: {totalMb}");
+    Console.WriteLine($"Part two: {totalMb}");
 }
 
-void RunRounds(int rounds, List<Monkey> monkeys)
+static long GetDivisor(List<Monkey> monkeys)
+{
+    var divisor = 1;
+    foreach (var monkey in monkeys)
+        divisor *= monkey.Divisor;
+
+    return divisor;
+}
+
+void RunRounds(int rounds, List<Monkey> monkeys, long divisor)
 {
     for (int i = 0; i < rounds; i++)
-        Round(monkeys);
+        Round(monkeys, divisor);
 }
 
-void Round(List<Monkey> monkeys)
+void Round(List<Monkey> monkeys, long divisor)
 {
     foreach (var monkey in monkeys)
     {
         if (monkey.Items.Any())
-            monkey.TakeTurn(monkeys);
-
+            monkey.TakeTurn(monkeys, divisor);
     }
 }
 
-void RunRounds2(int rounds, List<Monkey> monkeys)
-{
-    for (int i = 0; i < rounds; i++)
-        Round2(monkeys);
-}
-
-void Round2(List<Monkey> monkeys)
-{
-    foreach (var monkey in monkeys)
-    {
-        if (monkey.Items.Any())
-            monkey.TakeTurn2(monkeys);
-
-    }
-}
-
-List<Monkey> ParseMonkeys(string[] input)
+List<Monkey> ParseMonkeys(string[] input, bool partOne)
 {
     List<Monkey> monkeys = new List<Monkey>();
 
-    for (int i = 0; i < input.Length; i+=7)
+    for (int i = 0; i < input.Length; i += 7)
     {
         var segment = input.Skip(i).Take(6).ToList();
 
-        monkeys.Add(new Monkey(segment));
+        monkeys.Add(new Monkey(segment, partOne));
     }
     return monkeys;
 }
@@ -79,47 +73,36 @@ class Monkey
     public int Nr { get; }
     public long MbLevel { get; private set; }
     public List<long> Items { get; set; }
+    public int Divisor { get; private set; }
     private string Operation { get; }
-    private Func<long, long> Test { get; }
+    private Func<long, int> Test { get; }
+    private bool PartOne { get; }
 
-    public Monkey(List<string> input)
+    public Monkey(List<string> input, bool partOne)
     {
         Nr = int.Parse(input[0].Split(' ')[1][0].ToString());
         Items = input[1].Split("Starting items: ")[1].Split(", ").Select(x => long.Parse(x)).ToList();
         Operation = input[2].Split("Operation: new = ")[1];
 
-        var divisor = int.Parse(input[3].Split("Test: divisible by ")[1]);
+        Divisor = int.Parse(input[3].Split("Test: divisible by ")[1]);
         var tMonkey = int.Parse(input[4].Split("If true: throw to monkey ")[1]);
         var fMonkey = int.Parse(input[5].Split("If false: throw to monkey ")[1]);
         Test = (x) =>
         {
-            return x % divisor == 0 ? tMonkey : fMonkey;
+            return x % Divisor == 0 ? tMonkey : fMonkey;
         };
+        PartOne = partOne;
     }
 
-    public void TakeTurn(List<Monkey> monkeys)
+    public void TakeTurn(List<Monkey> monkeys, long divisor)
     {
         foreach (var item in Items)
         {
             MbLevel++;
 
-            var newWorryLevel = InspectItem(item);
-            newWorryLevel = (long)Math.Floor(newWorryLevel / 3.0);
-
-            var monkey = Test(newWorryLevel);
-
-            monkeys.First(x => x.Nr == monkey).Items.Add(newWorryLevel);
-        }
-        Items.Clear();
-    }
-
-    public void TakeTurn2(List<Monkey> monkeys)
-    {
-        foreach (var item in Items)
-        {
-            MbLevel++;
-
-            var newWorryLevel = InspectItem(item);
+            var newWorryLevel = InspectItem(item % divisor);
+            if (PartOne)
+                newWorryLevel = (long)Math.Floor(newWorryLevel / 3.0);
 
             var monkey = Test(newWorryLevel);
 
@@ -140,12 +123,8 @@ class Monkey
         {
             case "+":
                 return x + y;
-            case "-":
-                return x - y;
             case "*":
                 return x * y;
-            case "/":
-                return x / y;
             default:
                 break;
         }
