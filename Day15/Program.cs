fadb094987fa08a87422acaceba9b8e9c90330f6
Daniel <d.o.rasmusson@gmail.com>
2022-12-16 23:26:43 +0100

    Removed old code from day 15.

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,10 +1,5 @@
 ﻿// https://adventofcode.com/2022/day/15
 
-using System.Drawing;
-using System;
-using System.Diagnostics;
-using System.Timers;
-
 var input = File.ReadAllLines("Day15.txt");
 
 PartOne(input);
@@ -22,11 +17,7 @@ void PartOne(string[] input)
 void PartTwo(string[] input)
 {
 	var sensors = ParseToSensors(input);
-	var sc = new Stopwatch();
-	sc.Start();
 	var beacon = FindBeacon(sensors);
-	sc.Stop();
-	Console.WriteLine(sc.ElapsedMilliseconds / 100);
 	var answer = CalculateTuningFrequency(beacon);
 	Console.WriteLine($"Part two: {answer}");
 }
@@ -38,9 +29,7 @@ Coord FindBeacon(List<Sensor> sensors)
 		foreach (var edgeCoord in sensor.GetEdgeCoords(0, 4000000))
 		{
 			if (!InRange(sensors, edgeCoord))
-			{
 				return edgeCoord;
-			}
 		}
 	}
 	return new Coord(0, 0);
@@ -51,9 +40,7 @@ bool InRange(List<Sensor> sensors, Coord coord)
 	foreach (var sensor in sensors)
 	{
 		if (sensor.IsCoordWithinRange(coord))
-		{
 			return true;
-		}
 	}
 	return false;
 }
@@ -62,32 +49,6 @@ long CalculateTuningFrequency(Coord beacon)
 {
 	return (long)beacon.X * (long)4000000 + (long)beacon.Y;
 }
-
-//Coord Search(List<Coord> coordsToSearchFor, List<Sensor> sensors)
-//{
-//	var coordFound = false;
-//	var beacon = new Coord(0, 0);
-//	while (!coordFound)
-//	{
-//		var coordToSearchFor = coordsToSearchFor.First();
-
-//        foreach (var sensor in sensors)
-//        {
-//            if (!sensor.CoveredCoords.Contains(coordToSearchFor)) 
-//			{
-//				beacon = coordToSearchFor;
-//				coordFound = true;
-//			}
-//			else
-//			{
-//				coordsToSearchFor.Remove(coordToSearchFor);
-//				coordFound = false;
-//				break;
-//			}
-//        }
-//	}
-//	return beacon;
-//}
 
 List<Sensor> ParseToSensors(string[] input)
 {
@@ -106,20 +67,6 @@ List<Sensor> ParseToSensors(string[] input)
 	return sensors;
 }
 
-//List<Coord> GetAllPossibleCoords(int min, int max)
-//{
-//    var coords = new List<Coord>();
-
-//    for (int x = min; x <= max; x++)
-//    {
-//        for (int y = min; y <= max; y++)
-//        {
-//			coords.Add(new Coord(x, y));
-//        }
-//    }
-//	return coords;
-//}
-
 record Coord(int X, int Y)
 {
 	public int ManhattanDistanceFromCoord(Coord other) => Math.Abs(X - other.X) + Math.Abs(Y - other.Y);
@@ -128,15 +75,13 @@ record Coord(int X, int Y)
 class Sensor
 {
 	public Coord ClosestBeacon { get; private set; }
-	//public List<Coord> CoveredCoords { get; private set; }
+	public int Range { get; private set; }
 	private Coord Position { get; set; }
-	private int Range { get; set; }
 	public Sensor(int sensorX, int sensorY, int beaconX, int beaconY)
 	{
 		Position = new Coord(sensorX, sensorY);
         ClosestBeacon = new Coord(beaconX, beaconY);
 		Range = Position.ManhattanDistanceFromCoord(ClosestBeacon);
-		//CoveredCoords = GetCoveredCoords();
 	}
 
 	public List<Coord> GetCoordsOnRowWithinRange(int row)
@@ -154,15 +99,12 @@ class Sensor
 		return coords;
 	}
 
-	public HashSet<Coord> GetEdgeCoords(int min, int max)
+	public IEnumerable<Coord> GetEdgeCoords(int min, int max)
 	{
-        var coords = new HashSet<Coord>();
 		var outerEdgeDist = Range + 1;
 
 		int minx = Position.X - outerEdgeDist;
 		int maxx = Position.X + outerEdgeDist;
-		int miny = Position.Y - outerEdgeDist;
-		int maxy = Position.Y + outerEdgeDist;
 
         for (int i = minx; i <= maxx; i++)
         {
@@ -173,40 +115,18 @@ class Sensor
 			if (x >= min && x <= max &&
 				y1 >= min && y1 <= max)
 			{
-				coords.Add(new Coord(x, y1));
+				yield return new Coord(x, y1);
 			}
 			if (x >= min && x <= max &&
 				y2 >= min && y2 <= max)
 			{
-				coords.Add(new Coord(x, y2));
+				yield return new Coord(x, y2);
 			}
         }
-
-        return coords;
     }
 
     public bool IsCoordWithinRange(Coord other)
     {
 		return Position.ManhattanDistanceFromCoord(other) <= Range;
     }
-
-
-    //List<Coord> GetCoveredCoords()
-    //{
-    //       var coordinates = new List<Coord>();
-    //	for (int x = Position.X - Range; x <= Position.X + Range; x++)
-    //	{
-    //		for (int y = Position.Y - Range; y <= Position.Y + Range; y++)
-    //		{
-    //			var coordsoTest = new Coord(x, y);
-    //			if (IsWithinRange(coordsoTest))
-    //			{
-    //                   coordinates.Add(coordsoTest);
-    //               }
-    //           }
-    //       }
-
-    //       return coordinates;
-    //   }
-
 }
