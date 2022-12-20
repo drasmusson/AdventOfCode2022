582fad265f6fffcaedabde7c63a800334fbf3218
Daniel <d.o.rasmusson@gmail.com>
2022-12-20 21:24:06 +0100

    Day 17 part one.

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,31 +1,95 @@
 ﻿// https://adventofcode.com/2022/day/17
 
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-
 var input = File.ReadAllLines("Day17.txt");
 
 PartOne(input);
+PartTwo(input);
 
 void PartOne(string[] input)
 {
-    var chamber = new Chamber();
+    var rockDropper = new RockDropper(input[0]);
+    rockDropper.DropRocks(2022);
+    var answer = rockDropper.Chamber.HighestPoint.Y;
+    Console.WriteLine($"Part one: {answer}");
 }
 
-Block CreateBlock(int type, Coord startCoord) => (type) switch
+//void PartTwo(string[] input)
+//{
+//    var rockDropper = new RockDropper(input[0]);
+//    rockDropper.DropRocks(1000000000000);
+//    var answer = rockDropper.Chamber.HighestPoint.Y;
+//    Console.WriteLine($"Part two: {answer}");
+//}
+
+class RockDropper
 {
-    1 => new Line(startCoord),
-    2 => new Plus(startCoord),
-    3 => new L(startCoord),
-    4 => new I(startCoord),
-    5 => new Square(startCoord),
-};
+    public Chamber Chamber { get; private set; }
+    private string JetPattern { get; set; }
+    private int Iteration { get; set; }
+    public RockDropper(string jetPattern)
+    {
+        Chamber = new Chamber();
+        JetPattern = jetPattern;
+        Iteration = 0;
+    }
+
+    public void DropRocks(long rocks)
+    {
+        for (int i = 0; i < rocks; i++)
+        {
+            var rock = CreateRock(i % 5, GetNewStartingCoord());
+
+            DropRock(rock);
+        }
+    }
+
+    void DropRock(Rock rock)
+    {
+        var rockIsFalling = true;
+
+        while (rockIsFalling)
+        {
+            var direction = JetPattern[Iteration % JetPattern.Length];
+
+            rock.MoveInDirection(direction);
+            if (Chamber.IsIntersecting(rock.Body))
+                rock.UndoLastMove();
+
+            rock.MoveInDirection('V');
+            if (Chamber.IsIntersecting(rock.Body))
+            {
+                rock.UndoLastMove();
+                Chamber.AddRock(rock.Body);
+                rockIsFalling = false;
+            }
+            Iteration++;
+        }
+    }
+
+    Coord GetNewStartingCoord()
+    {
+        var highestCoord = Chamber.HighestPoint;
+        return Chamber.HighestPoint with { X = 2, Y = highestCoord.Y + 4 };
+    }
+
+    Rock CreateRock(int type, Coord startCoord) => (type) switch
+    {
+        0 => new Line(startCoord),
+        1 => new Plus(startCoord),
+        2 => new L(startCoord),
+        3 => new I(startCoord),
+        4 => new Square(startCoord),
+        _ => throw new NotImplementedException()
+    };
+
+}
 
 public record Coord(int X, int Y);
 
 class Chamber
 {
     public Dictionary<Coord, string> Map { get; private set; }
+    public Coord HighestPoint => Map.Keys.OrderByDescending(x => x.Y).First();
 
     public Chamber()
     {
@@ -38,7 +102,19 @@ class Chamber
         }
     }
 
-    public bool IsOnEdge(Coord coord)
+    public bool IsIntersecting(List<Coord> rock)
+    {
+        return rock.Any(Map.ContainsKey) ||
+            rock.Any(IsOnEdge);
+    }
+
+    public void AddRock(List<Coord> rock)
+    {
+        foreach (var coord in rock)
+            Map[coord] = "#";
+    }
+
+    bool IsOnEdge(Coord coord)
     {
         if (coord.X < 0) return true;
         if (coord.X > 6) return true;
@@ -47,12 +123,72 @@ class Chamber
     }
 }
 
-abstract class Block
+abstract class Rock
 {
-    public List<Coord> Body { get; set; }
+    public List<Coord> Body { get; internal set; }
+    private List<Coord> LastPosition { get; set; }
+
+    public void MoveInDirection(char direction)
+    {
+        switch (direction)
+        {
+            case '>':
+                MoveRight();
+                break;
+            case '<':
+                MoveLeft();
+                break;
+            case 'V':
+                MoveDown();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void UndoLastMove()
+    {
+        Body = LastPosition;
+    }
+
+    private void MoveDown()
+    {
+        LastPosition = Body;
+
+        var newPosition = new List<Coord>();
+
+        foreach (var coord in Body)
+            newPosition.Add(coord with { Y = coord.Y - 1 });
+
+        Body = newPosition;
+    }
+
+    private void MoveLeft()
+    {
+        LastPosition = Body;
+
+        var newPosition = new List<Coord>();
+
+        foreach (var coord in Body)
+            newPosition.Add(coord with { X = coord.X - 1 });
+
+        Body = newPosition;
+    }
+
+    private void MoveRight()
+    {
+        LastPosition = Body;
+
+        var newPosition = new List<Coord>();
+
+        foreach (var coord in Body)
+            newPosition.Add(coord with { X = coord.X + 1 });
+
+        Body = newPosition;
+    }
 }
 
-class Line : Block
+class Line : Rock
 {
     public Line(Coord startCoord)
     {
@@ -66,7 +202,7 @@ class Line : Block
     }
 }
 
-class Plus : Block
+class Plus : Rock
 {
     public Plus(Coord startCoord)
     {
@@ -81,7 +217,7 @@ class Plus : Block
     }
 }
 
-class L : Block
+class L : Rock
 {
     public L(Coord startCoord)
     {
@@ -96,7 +232,7 @@ class L : Block
     }
 }
 
-class I : Block
+class I : Rock
 {
     public I(Coord startCoord)
     {
@@ -110,7 +246,7 @@ class I : Block
     }
 }
 
-class Square : Block
+class Square : Rock
 {
     public Square(Coord startCoord)
     {
