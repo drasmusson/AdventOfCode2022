8e78cb32099910bdf95daeea3a29bee37077bd40
Daniel <d.o.rasmusson@gmail.com>
2022-12-27 10:56:44 +0100

    Started on day 19. Worked in vscode and added vscode files to .gitignore.

---
+++ Day19/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://adventofcode.com/2022/day/19
+
+var input = File.ReadAllLines("../Input/Day19.txt");
+
+PartOne(input);
+
+void PartOne(string[] input)
+{
+    
+}
+
+record Robot(
+    List<(Unit, int)> Cost,
+    (Unit, int) Production
+);
+
+enum Unit
+{
+    Ore,
+    Clay,
+    Obsidian,
+    Geode
+}
