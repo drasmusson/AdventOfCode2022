0d31cac6544853052a934c482ce65e62e618f6ff
Daniel <d.o.rasmusson@gmail.com>
2022-12-28 16:50:46 +0100

    Input parsing for day 19.

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,17 +1,51 @@
 ﻿// https://adventofcode.com/2022/day/19
 
-var input = File.ReadAllLines("../Input/Day19.txt");
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("Day19.txt");
 
 PartOne(input);
 
 void PartOne(string[] input)
 {
-    
+    var blueprints = ParseBlueprints(input);
+}
+
+List<Blueprint> ParseBlueprints(string[] input)
+{
+    var blueprints = new List<Blueprint>();
+
+    foreach (var line in input)
+    {
+        var nums = Regex.Matches(line, @"\d+").Select(x => int.Parse(x.Value)).ToArray();
+
+        blueprints.Add(new Blueprint(
+            nums[0],
+            new Robot(Unit.Ore, new List<(Unit, int)> { (Unit.Ore, nums[1]) }),
+            new Robot(Unit.Clay, new List<(Unit, int)> { (Unit.Ore, nums[2]) }),
+            new Robot(Unit.Obsidian, new List<(Unit, int)> { (Unit.Ore, nums[3]), (Unit.Clay, nums[4]) }),
+            new Robot(Unit.Geode, new List<(Unit, int)> { (Unit.Ore, nums[5]), (Unit.Obsidian, nums[6]) })
+            )
+        );
+    }
+    return blueprints;
+}
+
+class Blueprint
+{
+    public int Id { get; private set; }
+    public Robot[] Robots { get; private set; }
+
+    public Blueprint(int id, params Robot[] robots)
+    {
+        Id = id;
+        Robots = robots;
+    }
 }
 
 record Robot(
-    List<(Unit, int)> Cost,
-    (Unit, int) Production
+    Unit Production,
+    List<(Unit, int)> Cost
 );
 
 enum Unit
