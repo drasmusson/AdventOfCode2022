563a7fc475757ebcc1240b5f62da0958357b302b
Daniel <d.o.rasmusson@gmail.com>
2023-01-07 22:10:05 +0100

    Day 21 finally done.

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -2,7 +2,7 @@
 
 var input = File.ReadAllLines("Day21.txt");
 
-//PartOne(input);
+PartOne(input);
 PartTwo(input);
 
 void PartOne(string[] input)
@@ -19,26 +19,27 @@ void PartOne(string[] input)
 void PartTwo(string[] input)
 {
     var monkeys = ParseMonkeys(input);
-
+    monkeys.First(x => x.Name == "humn").Number = null;
     var root = monkeys.First(x => x.Name == "root");
 
     GetAndSetNumber(monkeys, root);
 
-    var path = GetPathToHumn(monkeys, root, "");
-
-    Console.WriteLine($"Part one: {path}");
+    var path = GetHumnPath(monkeys, root, new List<Operation>());
+    var startValue = path.First().LeftValue == null ? path.First().RightValue : path.First().LeftValue;
+    var answer = EvaluateFormula(path.Skip(1).ToList(), startValue.Value);
+    Console.WriteLine($"Part two: {answer}");
 }
 
-void SetNumbersForAllButHumnAndParents(List<Monkey> monkeys, Monkey root)
+long EvaluateFormula(List<Operation> values, long startValue)
 {
-    var humnFree = monkeys.Where(x => x.Name == root.Formula.MonkeyOne || x.Name == root.Formula.MonkeyTwo).First(m => !DoesMonkeyReachHumn(monkeys, m));
+    var humn = startValue;
 
-    var rootNumber = GetAndSetNumber(monkeys, humnFree);
-
-    var b = monkeys.First(x => x.Formula != null && (x.Formula.MonkeyOne == "humn" || x.Formula.MonkeyTwo == "humn"));
-    var humnBrother = b.Formula.MonkeyOne == "humn" ? b.Formula.MonkeyTwo : b.Formula.MonkeyOne;
-
-    var p2 = GetAndSetNumber(monkeys, monkeys.First(x => x.Name == humnBrother));
+    foreach (var operation in values)
+    {
+        if (operation.Operator == "humn") return humn;
+        humn = operation.CalculateInverse(humn);
+    }
+    return humn;
 }
 
 List<Monkey> ParseMonkeys(string[] input)
@@ -67,7 +68,7 @@ long GetNumber(List<Monkey> monkeys, Monkey monkey)
 
     var number = 0L;
 
-    switch (monkey.Formula.Modifier)
+    switch (monkey.Formula.Operator)
     {
         case "+":
             number = GetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyOne)) + GetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo));
@@ -98,13 +99,12 @@ long? GetAndSetNumber(List<Monkey> monkeys, Monkey monkey)
     var rightMonkeyNumber = GetAndSetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo));
 
     if (leftMonkeyNumber is null || rightMonkeyNumber is null)
-    {
         return null;
-    }
-    switch (monkey.Formula.Modifier)
+
+    switch (monkey.Formula.Operator)
     {
         case "+":
-            number = GetAndSetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyOne)) + GetAndSetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo)).Value;
+            number = GetAndSetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyOne)) + GetAndSetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo));
             break;
         case "-":
             number = GetAndSetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyOne)) - GetAndSetNumber(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo));
@@ -122,29 +122,85 @@ long? GetAndSetNumber(List<Monkey> monkeys, Monkey monkey)
     return number;
 }
 
-string GetPathToHumn(List<Monkey> monkeys, Monkey monkey, string path)
+List<Operation> GetHumnPath(List<Monkey> monkeys, Monkey monkey, List<Operation> formula)
 {
-    if (monkey.Name == "humn") return path += ", " + monkey.Name;
-    if (monkey.Number.HasValue) return "";
+    if (monkey.Name == "humn")
+    {
+        formula.Add(new Operation(null, "humn", null));
+        return formula;
+    }
 
-    path += ", " + monkey.Name;
-    var p1 = GetPathToHumn(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyOne), path);
-    var p2 = GetPathToHumn(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo), path);
+    var monkey1 = monkeys.First(x => x.Name == monkey.Formula.MonkeyOne);
+    var monkey2 = monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo);
 
-    if (p1 != "") return p1;
+    if (monkey1.Number == null)
+    {
+        formula.Add(new Operation(null, monkey.Formula.Operator, monkey2.Number));
+        GetHumnPath(monkeys, monkey1, formula);
+    }
+    else
+    {
+        formula.Add(new Operation(monkey1.Number, monkey.Formula.Operator, null));
+        GetHumnPath(monkeys, monkey2, formula);
+    }
 
-    return p2;
+    return formula;
 }
 
-bool DoesMonkeyReachHumn(List<Monkey> monkeys, Monkey monkey)
+class Operation
 {
-    if (monkey.Name == "humn") return true;
-    if (monkey.Number.HasValue) return false;
+    public long? LeftValue { get; }
+    public string Operator { get; }
+    public long? RightValue { get; }
 
-    var p1 = DoesMonkeyReachHumn(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyOne));
-    var p2 = DoesMonkeyReachHumn(monkeys, monkeys.First(x => x.Name == monkey.Formula.MonkeyTwo));
+    public Operation(long? leftValue, string @operator, long? rightValue)
+    {
+        LeftValue = leftValue;
+        Operator = @operator;
+        RightValue = rightValue;
+    }
 
-    return p1 || p2;
+    public long CalculateInverse(long a)
+    {
+        var b = LeftValue is null ? RightValue.Value : LeftValue.Value;
+
+        if (LeftValue != null) return CalculateRightInverse(a, b);
+        return CalculateLeftInverse(a, b);
+    }
+
+    private long CalculateLeftInverse(long a, long right)
+    {
+        switch (Operator)
+        {
+            case "+":
+                return a - right;
+            case "-":
+                return a + right;
+            case "*":
+                return a / right;
+            case "/":
+                return a * right;
+            default:
+                return a;
+        }
+    }
+
+    private long CalculateRightInverse(long a, long left)
+    {
+        switch (Operator)
+        {
+            case "+":
+                return a - left;
+            case "-":
+                return left - a;
+            case "*":
+                return a / left;
+            case "/":
+                return left * a;
+            default:
+                return a;
+        }
+    }
 }
 
 class Monkey
@@ -164,13 +220,13 @@ class Monkey
 
 class Formula
 {
-    public string Modifier { get; }
+    public string Operator { get; }
     public string MonkeyOne { get; }
     public string MonkeyTwo { get; }
 
     public Formula(string modifier, string monkeyOne, string monkeyTwo)
     {
-        Modifier = modifier;
+        Operator = modifier;
         MonkeyOne = monkeyOne;
         MonkeyTwo = monkeyTwo;
     }
