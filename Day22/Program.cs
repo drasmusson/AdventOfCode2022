6495c1305a8b89b9df79f856529cac6b8a0ac8ba
Daniel <d.o.rasmusson@gmail.com>
2023-02-01 10:36:44 +0100

    Day 22, part one, done.

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,6 +1,7 @@
 ﻿// https://adventofcode.com/2022/day/22
 
 using System.Data;
+using System.Text.RegularExpressions;
 
 var input = File.ReadAllLines("Day22.txt");
 
@@ -9,29 +10,145 @@ PartOne(input);
 void PartOne(string[] input)
 {
     var board = new Board(input.TakeWhile(x => !string.IsNullOrEmpty(x)).ToArray());
-    var instructions = input.Skip(input.ToList().IndexOf("") + 1).First();
+    var finger = new Finger(board.Start);
+    var instructionString = input.Skip(input.ToList().IndexOf("") + 1).First();
+    var pattern = @"(\d+)|(\D+)";
+    var instructions = Regex.Split(instructionString, pattern).Where(x => !string.IsNullOrEmpty(x)).ToList();
+    var finalPosition = MoveFingerAcrossBoard(board, finger, instructions);
+    var result = (1000 * finalPosition.Y) + (4 * finalPosition.X) + finger.Direction;
+
+    Console.Write($"Part one: {result}");
 }
 
-Coord MoveFinger(Finger finger, Board board, int steps)
+Coord MoveFingerAcrossBoard(Board board, Finger finger, List<string> instructions)
 {
-    for (int i = 0; i < steps; i++)
+    foreach (var instruction in instructions)
     {
-        switch (finger.Direction)
+        if (IsMoveInstruction(instruction))
         {
-            case Direction.Right:
-                break;
-            case Direction.Down:
-                break;
-            case Direction.Left:
-                break;
-            case Direction.Up:
-                break;
-            default:
-                break;
+            finger.Position = MoveAcrossBoard(board, finger.Direction, finger.Position, int.Parse(instruction));
+        }
+        else
+        {
+            finger.Turn(instruction);
         }
     }
 
-    return new Coord(0, 0);
+    return finger.Position;
+}
+
+bool IsMoveInstruction(string instruction) => int.TryParse(instruction, out var _);
+
+Coord MoveAcrossBoard(Board board, Direction direction, Coord start, int steps)
+{
+    var result = new Coord(0, 0);
+
+    switch (direction)
+    {
+        case Direction.Right:
+            result = MoveRight(board, start, steps);
+            break;
+        case Direction.Down:
+            result = MoveDown(board, start, steps);
+            break;
+        case Direction.Left:
+            result = MoveLeft(board, start, steps);
+            break;
+        case Direction.Up:
+            result = MoveUp(board, start, steps);
+            break;
+        default:
+            break;
+    }
+    return result;
+}
+
+Coord MoveRight(Board board, Coord start, int steps)
+{
+    var prev = start;
+
+    for (int i = 0; i < steps; i++)
+    {
+        var next = prev with { X = prev.X + 1 };
+        if (board.IsCoordOb(next))
+        {
+            var col = board.Map.Keys.Where(x => x.Y == next.Y);
+            next = col.First(c => c.X == col.Min(v => v.X));
+        }
+
+        if (board.Map[next] == '#')
+            return prev;
+
+        prev = next;
+    }
+
+    return prev;
+}
+
+Coord MoveLeft(Board board, Coord start, int steps)
+{
+    var prev = start;
+
+    for (int i = 0; i < steps; i++)
+    {
+        var next = prev with { X = prev.X - 1 };
+        if (board.IsCoordOb(next))
+        {
+            var col = board.Map.Keys.Where(x => x.Y == next.Y);
+            next = col.First(c => c.X == col.Max(v => v.X));
+        }
+
+        if (board.Map[next] == '#')
+            return prev;
+
+        prev = next;
+    }
+
+    return prev;
+}
+
+Coord MoveDown(Board board, Coord start, int steps)
+{
+    var prev = start;
+
+    for (int i = 0; i < steps; i++)
+    {
+        var next = prev with { Y = prev.Y + 1 };
+        if (board.IsCoordOb(next))
+        {
+            var row = board.Map.Keys.Where(x => x.X == next.X);
+            next = row.First(c => c.Y == row.Min(v => v.Y));
+        }
+
+        if (board.Map[next] == '#')
+            return prev;
+
+        prev = next;
+    }
+
+    return prev;
+}
+
+Coord MoveUp(Board board, Coord start, int steps)
+{
+    var prev = start;
+
+    for (int i = 0; i < steps; i++)
+    {
+        var next = prev with { Y = prev.Y - 1 };
+        if (board.IsCoordOb(next))
+        {
+            var row = board.Map.Keys.Where(x => x.X == next.X);
+            next = row.First(c => c.Y == row.Max(v => v.Y));
+        }
+
+        if (board.Map[next] == '#')
+            return prev;
+
+        prev = next;
+    }
+
+    return prev;
 }
 
 record Coord(int X, int Y);
@@ -61,12 +178,17 @@ class Finger
 
 class Board
 {
-    public Dictionary<Coord, char>  Map { get; }
+    public Dictionary<Coord, char> Map { get; }
     public Coord Start => GetStart();
 
     public Board(string[] input)
     {
         Map = InitMap(input);
+    }
+
+    public bool IsCoordOb(Coord coord)
+    {
+        return !Map.ContainsKey(coord);
     }
 
     private Coord GetStart()
